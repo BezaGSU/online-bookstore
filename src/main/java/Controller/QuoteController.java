0b7080926eb87dcfe150dc1e27030a9a90a2e59e
Mulualem <beza.c.mulualem@accenturefederal.com>
2020-11-11 12:24:12 -0500

    First commit

---
+++ src/main/java/Controller/QuoteController.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class QuoteController {
+	
+	@GetMapping
+	public String BeginQuoteRequest() {
+		
+		return "newQuote";
+	}
+	
+	
+	@PostMapping
+	public String SubmitQuoteRequest() {
+		
+		
+		return "newQuoteConfirmation";
+	}
+
+	
+}
